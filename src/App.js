29837f5e67c25d8654bf9145b342d46d7a188645
GianlorenzoDercole <johndercole01@gmail.com>
2022-10-27 12:30:39 -0700

    addded routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+// import { BrowserRouter, Routes, Route} from 'reac-router-dom'
 import './App.css';
 import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Services from './components/pages/Services'
 function App() {
   return (
-    <div className="App">
 
-      <Home />
-      <Contact />
-      <Services />
-    </div>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='services' element={<Services />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
